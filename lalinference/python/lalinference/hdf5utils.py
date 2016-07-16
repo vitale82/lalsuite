abfc2846d8389ba0967ac479f2c665ea44aba4a8
John Veitch <john.veitch@ligo.org>
2016-07-16 17:41:12 +0100

    hdf5utils.py: New utility function for array

--- lalinference/python/lalinference/hdf5utils.py
+++ lalinference/python/lalinference/hdf5utils.py
@@ -16,7 +16,7 @@ def get_posterior_group(h5path):
 def get_nested_samples_group(h5path):
     return h5_get_group(h5path,groupname=lalinference.LALInferenceHDF5NestedSamplesGroupName)
 
-def group_to_ndarray(h5grp,fill_attributes=False):
+def group_to_array(h5grp,fill_attributes=False):
     """
     Convert the given group to a numpy structured ndarray
     h5grp must contain 1 or more datasets of equal length, which form
@@ -51,10 +51,23 @@ def load_chain(h5path,groupname,fill_attributes=True):
     """
     from pylal import bayespputils as bppu
     from numpy import array
-    arr=group_to_ndarray(get_group(h5path,groupname), fill_attributes=fill_attributes)
+    arr=group_to_array(get_group(h5path,groupname), fill_attributes=fill_attributes)
     params=arr.dtype.names
     p=bppu.Posterior((params,array(zip(*arr))))
     return p
+
+def load_posterior_array(filename,fill_attributes=False):
+  """
+  Reads the posterior samples group from a given file
+  Returns a numpy structured array
+  """
+  import h5py
+  with h5py.File(filename,'r') as h5file:
+    return group_to_array( \
+      get_group(h5path \
+	, groupname=lalinference.LALInferenceHDF5PosteriorSamplesGroupName) \
+      , fill_attributes=fill_attributes)
+  return None
 
 def load_posterior_from_file(filename):
     """
@@ -64,3 +77,4 @@ def load_posterior_from_file(filename):
     import h5py
     with h5py.File(filename,'r') as h5file:
         return load_chain(h5file,groupname=lalinference.LALInferenceHDF5PosteriorSamplesGroupName,fill_attributes=True)
+    return None
